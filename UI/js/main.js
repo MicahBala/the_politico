95659e9bb26de50deec3ca57e8161b248332ec62
Micah Y. Bala <micahkulien@gmail.com>
2019-01-24 20:05:16 +0100

    Added password reset link and updated vot and register link

--- UI/js/main.js
+++ UI/js/main.js
@@ -5,12 +5,14 @@ let signupTab = document.querySelector(".create");
 let loginTab = document.querySelector(".login");
 let confirmInput = document.querySelector(".hide");
 let btnSubmit = document.querySelector(".btnSubmit");
+let passwordReset = document.querySelector(".pw-reset");
 
 let signUpActive = true;
 
 // Default appearance for unregistered user
 signupTab.classList.add("darkBg");
 loginTab.classList.add("lightBg");
+passwordReset.setAttribute("style", "display: none");
 
 // Event listeners
 loginTab.addEventListener("click", () => {
@@ -23,6 +25,7 @@ loginTab.addEventListener("click", () => {
 
     btnSubmit.innerText = "Log in";
     confirmInput.setAttribute("style", "display: none");
+    passwordReset.setAttribute("style", "display: flex");
   }
   signUpActive = false;
 });
@@ -37,6 +40,7 @@ signupTab.addEventListener("click", () => {
 
     btnSubmit.innerText = "Sign up";
     confirmInput.setAttribute("style", "display: block");
+    passwordReset.setAttribute("style", "display: none");
   }
   signUpActive = true;
 });
