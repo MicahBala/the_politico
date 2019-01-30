c8e10bba9a60362e9b956b3f80f77f35b3e7aa28
Micah Y. Bala <micahkulien@gmail.com>
2019-01-30 23:56:19 +0100

    [Feature #163583923] Added api endpoinyt to get parties and added unit test

--- api/routes/parties.js
+++ api/routes/parties.js
@@ -35,4 +35,18 @@ router.post('/', (req, res) => {
   res.send(createdParty);
 });
 
+// GET all PARTIES
+router.get('/', (req, res) => {
+  res.send(partyList);
+});
+
+// GET a single PARTY
+router.get('/:id', (req, res) => {
+  const party = partyList.filter(c => c.id === parseInt(req.params.id));
+
+  // If does not find a match return 404
+  if (!party) res.status(400).send('party not found');
+  res.send(party);
+});
+
 module.exports = router;
--- api/test/apitests.js
+++ api/test/apitests.js
@@ -1,3 +1,4 @@
+'use strict';
 let request = require('supertest');
 let app = require('../index.js');
 
@@ -30,6 +31,40 @@ describe('Create parties', () => {
         hqAddress: 'Kaduna',
         logoUrl: '/api/images/logo.png'
       })
+      .expect(200)
+      .expect(
+        {
+          id: 1,
+          name: 'PDP',
+          hqAddress: 'Kampala Street',
+          logoUrl: '/api/images/logo.png'
+        },
+        done()
+      );
+  });
+});
+
+describe('Get a list of parties', () => {
+  it('Get list of political parties', done => {
+    request(app)
+      .get('/parties')
+      .expect(200)
+      .expect(
+        {
+          id: 1,
+          name: 'PDP',
+          hqAddress: 'Kampala Street',
+          logoUrl: '/api/images/logo.png'
+        },
+        done()
+      );
+  });
+});
+
+describe('Get a single party', () => {
+  it('Get a single political party based on ID', done => {
+    request(app)
+      .get('/parties/:id')
       .expect(200)
       .expect(
         {
